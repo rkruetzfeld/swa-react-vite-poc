b7e60e8605a791cadb6b4fd3d1167009b1b14d8a
rkruetzfeld <146999031+rkruetzfeld@users.noreply.github.com>
2026-01-12 12:08:37 -0700

    Convert app to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: "system-ui, Arial" }}>
+      <h1>Hello World</h1>
+      <p>Now running TypeScript.</p>
+    </div>
+  );
+}
