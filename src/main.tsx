f1278467b83db4f1705a9ca16fb2a618c7c03586
rkruetzfeld <146999031+rkruetzfeld@users.noreply.github.com>
2026-01-13 08:07:15 -0700

    Register AG Grid community modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
+
+import { ModuleRegistry, AllCommunityModule } from "ag-grid-community";
+ModuleRegistry.registerModules([AllCommunityModule]);
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
